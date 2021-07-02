144738b804c50c46e7af39b518f30ba060a5d60a
LegItMate <77503777+LegItMate@users.noreply.github.com>
2021-07-02 20:59:08 +0530

    Initial commit

---
+++ commands/create-dp.js
@@ -0,0 +1,58 @@
+// require Nuggies
+const Nuggies = require('nuggies');
+const Discord = require('discord.js');
+/**
+ * @param {Discord.Client} client
+ * @param {Discord.Message} message
+ * @param {String[]} args
+ */
+
+module.exports.run = async (client, message, args) => {
+	const dpmanager = new Nuggies.dropdownroles();
+	message.channel.send('Send messages in `roleID label emoji` syntax! Once finished say `done`.');
+
+	/**
+	 * @param {Discord.Message} m
+	 */
+	const filter = m => m.author.id === message.author.id;
+	const collector = message.channel.createMessageCollector(filter, { max: 10000 });
+
+	collector.on('collect', async (msg) => {
+		if (!msg.content) return message.channel.send('Invalid syntax');
+		if (msg.content.toLowerCase() == 'done') return collector.stop('DONE');
+		if (!msg.content.split(' ')[0].match(/[0-9]{18}/g)) return message.channel.send('Invalid syntax');
+
+		const role = msg.content.split(' ')[0];
+		// const role = message.guild.roles.cache.get(roleid);
+		if (!role) return message.channel.send('Invalid role');
+
+		const label = msg.content.split(' ').slice(1, msg.content.split(' ').length - 1).join(' ');
+
+		const reaction = (await msg.react(msg.content.split(' ').slice(msg.content.split(' ').length - 1).join(' ')).catch(/*() => null*/console.log));
+
+		const final = {
+			role, label, emoji: reaction ? reaction.emoji.id || reaction.emoji.name : null,
+		};
+		dpmanager.addrole(final);
+	})
+
+	collector.on('end', async (msgs, reason) => {
+		if (reason == 'DONE') {
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Dropdown roles!')
+				.setDescription('Click on the buttons to get the specific role or vice-versa')
+				.setColor('RANDOM')
+				.setTimestamp();
+			Nuggies.dropdownroles.create({ message, content: embed, role: dpmanager, channelID: message.channel.id })
+		}
+	});
+};
+
+module.exports.config = {
+	name: 'create-dp',
+	description: 'Creates dropdown role!',
+	usage: '?create-dp',
+	botPerms: [],
+	userPerms: ['MANAGE_GUILD'],
+	aliases: [],
+};
