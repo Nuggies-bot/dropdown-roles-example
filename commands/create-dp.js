25a8ba91bb5719383f256fbe65c1a327d52e93a7
Assassin#1234 <chordsandfrets@gmail.com>
2021-07-05 09:10:35 +0530

    fixed all kind of errors

--- commands/create-dp.js
+++ commands/create-dp.js
@@ -8,7 +8,8 @@ const Discord = require('discord.js');
  */
 
 module.exports.run = async (client, message, args) => {
-	const dpmanager = new Nuggies.dropdownroles();
+	if(!message.member.hasPermission('MANAGE_SERVER')) return message.reply('You do not have the permission \`MANAGE_SERVER\`');
+    const dpmanager = new Nuggies.dropdownroles();
 	message.channel.send('Send messages in `roleID label emoji` syntax! Once finished say `done`.');
 
 	/**
@@ -22,8 +23,8 @@ module.exports.run = async (client, message, args) => {
 		if (msg.content.toLowerCase() == 'done') return collector.stop('DONE');
 		if (!msg.content.split(' ')[0].match(/[0-9]{18}/g)) return message.channel.send('Invalid syntax');
 
-		const role = msg.content.split(' ')[0];
-		// const role = message.guild.roles.cache.get(roleid);
+		const roleid = msg.content.split(' ')[0];
+		const role = message.guild.roles.cache.get(roleid);
 		if (!role) return message.channel.send('Invalid role');
 
 		const label = msg.content.split(' ').slice(1, msg.content.split(' ').length - 1).join(' ');
@@ -31,19 +32,19 @@ module.exports.run = async (client, message, args) => {
 		const reaction = (await msg.react(msg.content.split(' ').slice(msg.content.split(' ').length - 1).join(' ')).catch(/*() => null*/console.log));
 
 		const final = {
-			role, label, emoji: reaction ? reaction.emoji.id || reaction.emoji.name : null,
+			role: roleid, label: label, emoji: reaction ? reaction.emoji.id || reaction.emoji.name : null,
 		};
 		dpmanager.addrole(final);
 	})
 
 	collector.on('end', async (msgs, reason) => {
 		if (reason == 'DONE') {
-			const embed = new Discord.MessageEmbed()
+			const embed = new MessageEmbed()
 				.setTitle('Dropdown roles!')
 				.setDescription('Click on the buttons to get the specific role or vice-versa')
 				.setColor('RANDOM')
 				.setTimestamp();
-			Nuggies.dropdownroles.create({ message, content: embed, role: dpmanager, channelID: message.channel.id })
+			Nuggies.dropdownroles.create({ message: message, content: embed, role: dpmanager, channelID: message.channel.id })
 		}
 	});
 };
